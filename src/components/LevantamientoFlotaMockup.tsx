a81828daf4fe3ae7b65b11836bf08408e9e3a3d5
KennyDzul <163344883+KennyDzul@users.noreply.github.com>
2026-09-17 13:48:00 -0600

    ulitmo

--- src/components/LevantamientoFlotaMockup.tsx
+++ src/components/LevantamientoFlotaMockup.tsx
@@ -12,7 +12,7 @@ const LevantamientoFlotaMockup: React.FC = () => {
         { id: 'electrico', label: 'Eléctrico/Interiores' },
         { id: 'equipo', label: 'Equipo Adicional' },
     ];
-
+//hola
     return (
         <div className={styles.mockupContainer}>
             {/* WATERMARK */}
